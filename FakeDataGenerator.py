b335d09cbf527278ec1b9594b82c74d4fae39bbc
tares <stareq13@yahoo.com>
2019-11-26 01:36:56 +0000

    Created which generates random data

---
+++ FakeDataGenerator.py
@@ -0,0 +1,66 @@
+import csv
+import random
+
+from faker import Faker
+
+fake = Faker()
+
+user_registered = [True, False]
+
+
+def getRandomDetails():
+    person = dict()
+
+    if not i % 2 == 0:
+        person["name"] = fake.first_name_male() + " " + fake.last_name_male()
+    else:
+        person["name"] = fake.first_name_female() + " " + fake.last_name_female()
+
+    person["Registered"] = random.choice(user_registered)
+    person["dob"] = "".join(
+        map(str, [random.randint(1, 30), "/", random.randint(1, 12), "/", random.randint(1940, 2002)]))
+    person["id"] = "".join(map(str, [person["name"].split()[0], random.randint(0, 50000)]))
+    return person
+
+
+with open("RandomStudents.csv", "w", newline='') as file:
+    writer = csv.DictWriter(file, fieldnames=["name", "Registered", "dob", "id"], quoting=csv.QUOTE_ALL)
+    header_written = False
+    for i in range(1000):
+        if not header_written:
+            writer.writeheader()
+            header_written = True
+        student = getRandomDetails()
+        writer.writerow(student)
+
+random_choice_candadates = list()
+postions = {"GSU Officers": 3,
+            "President": 1,
+            "Faculty Officer": 16
+            }
+
+with open("RandomStudents.csv", "r") as students_details_file:
+    csv_reader = csv.DictReader(students_details_file)
+    all_Students = []
+    for row in csv_reader:
+        all_Students.append(dict(row))
+
+    for key, val in postions.items():  # Each position
+        for i in range(val):
+            unique = False
+            while not unique:
+                choice = random.choice(all_Students)
+                if choice not in random_choice_candadates:
+                    choice["position"] = key
+                    random_choice_candadates.append(choice)
+                    unique = True
+                else:
+                    continue
+with open("RandomCandidates.csv", "w", newline="") as f:
+    writer = csv.DictWriter(f, fieldnames=["name", "Registered", "dob", "id", "position"], quoting=csv.QUOTE_ALL)
+    header_written = False
+    for i in random_choice_candadates:
+        if not header_written:
+            writer.writeheader()
+            header_written = True
+        writer.writerow(i)
