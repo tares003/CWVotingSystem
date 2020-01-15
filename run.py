2cdeb9861218402c0b11b7acc469a67fe40a6662
RoozM1 <58185718+RoozM1@users.noreply.github.com>
2020-01-15 15:10:04 +0000

    Update run.py

--- run.py
+++ run.py
@@ -17,6 +17,7 @@ def tk_gui():
 
 
 if __name__ == "__main__":
+    
     app.config.from_object("config.DevelopmentConfig")
 
     read_student_text_file('voting_system/RandomStudents.csv',remove_dups=True)
@@ -32,3 +33,80 @@ if __name__ == "__main__":
     except:
         pass
     app.run(port=2222)
+
+    
+from tkinter import Frame, Tk, BOTH, Text, Menu, END, filedialog
+
+class Example(Frame):
+
+    def __init__(self, parent):
+        Frame.__init__(self, parent)
+
+        self.parent = parent
+        self.initUI()
+
+    def initUI(self):
+
+        self.parent.title("File dialog")
+        self.pack(fill=BOTH, expand=1)
+
+        menubar = Menu(self.parent)
+        self.parent.config(menu=menubar)
+
+        fileMenu = Menu(menubar)
+        fileMenu.add_command(label="open candidates", command=self.onOpen)
+        menubar.add_cascade(label="open students", menu=fileMenu)
+
+        self.txt = Text(self)
+        self.txt.pack(fill=BOTH, expand=1)
+
+
+    def onOpen(self):
+
+        ftypes = [('Python files', '*.py'), ('All files', '*')]
+        dlg = filedialog.Open(self, filetypes = ftypes)
+        fl = dlg.show()
+
+        if fl != '':
+            text = self.readFile(fl)
+            self.txt.insert(END, text)
+
+    def readFile(self, filename):
+
+        f = open(filename, "r")
+        text = f.read()
+        return text
+
+
+def main():
+
+    root = Tk()
+    ex = Example(root)
+    root.geometry("300x250+300+300")
+    root.mainloop()  
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+# import tkinter
+# from  tkinter import filedialog
+#
+#
+# if __name__== "__main__":
+#     window = tkinter.Tk()
+#     window.file_candidates = filedialog.askopenfilename(initialdir="/", title="Open Candidates",
+#                                                  filetypes = (("csv files","*.csv"),("all files","*.*")))
+#     print(window.file_candidates)
+#     window.file_students = filedialog.askopenfilename(initialdir="/", title="Open Candidates",
+#                                                  filetypes = (("csv files","*.csv"),("all files","*.*")))
+#     print(window.file_students)
+#     window.title("Button GUI")
+#     button_widget = tkinter.Button(window, text="Start Server")
+#     button_widget.pack()
+#     button_widget = tkinter.Button(window, text="Stop Server")
+#     button_widget.pack()
+#     tkinter.mainloop()
+    
