beacbf197016ce7ade0f0912275592292ad9ec17
tares0003 <crisp003>
2020-01-25 17:15:19 +0000

    all

--- flask_server.py
+++ flask_server.py
@@ -10,11 +10,36 @@ from voting_system.VotingSystem import app, read_student_text_file, read_candada
 def tk_gui():
     pass
 
+# TODO: Get the vote selction
+# TODO: Store it in local DB
+# TODO: Count vote with tie considaration
+# todo:
 
+# import pandas
+# import re
+#
+# pd_df = pandas.read_csv('fred.csv', header=0)
+#
+# def filter_by_position(position):
+#     return pd_df[pd_df.position.str.lower() == position.lower()]
+#
+#
+# def sort_by_cadidates(df):
+#     return df.sort_values(['first_votes','second_votes','third_votes','fourth_votes'], ascending=[True, True, True, True])
+#
+#
+#
+# user_fileter = str(input('Enter the position to filter by:'))
+#
+# ##print(filter_by_position('President'))
+#
+# filtered = filter_by_position(user_fileter)
+# print(sort_by_cadidates(filtered))
+# #
+# # print(pd_df[pd_df.position == 'President'])
+#
 if __name__ == "__main__":
-
     app.config.from_object("config.DevelopmentConfig")
-
     read_student_text_file('voting_system/RandomStudents.csv', remove_dups=True)
     read_candadates_text_file('voting_system/RandomCandidates.csv', remove_dups=True)
 
